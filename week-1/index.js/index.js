1169febd83ca3b30401d3845f42a229c44fbf326
Kerben Bermingham <b.kerben2@gmail.com>
2019-04-03 22:25:15 +0000

    Automatically backed up by Learn

---
+++ week-1/index.js/index.js
@@ -0,0 +1,15 @@
+let tacosAvailable = 3 
+let weather = 'sunny'
+
+if (weather === 'sunny' && tacosAvailable > 0) {
+  alert('time for taco tuesday')
+
+if(tacosAvailable === 2) {
+  alert("there's 2 left!")
+} else if (tacosAvailable > 5) {
+  alert('5 omg')
+} else {
+  alert('1 left')
+}
+
+}
---
+++ week-1/index.js/week-5/index.js
@@ -0,0 +1,14 @@
+function fiftyDiscountPrice(nonDiscountPrice){
+  return nonDiscountPrice / 2
+}
+
+function makeANumberAPrice(someNumber) {
+  console.log('$' + someNumber + '.00') 
+}
+
+const price = 20
+const discountedPrice = fiftyDiscountPrice(Price)
+
+makeANumberAPrice(34)
+makeANumberAPrice(56)
+makeANumberAPrice(22)
